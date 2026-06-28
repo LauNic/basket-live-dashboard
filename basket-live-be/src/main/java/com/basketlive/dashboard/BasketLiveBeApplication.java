acc88b24ee163ee9dbf1f22faec12442cfb48e5f
Laurentiu Octavian Niculescu <niculesculaurentiu@yahoo.com>
2026-06-28 18:18:18 +0300

    Create spring boot skeleton

---
+++ basket-live-be/src/main/java/com/basketlive/dashboard/BasketLiveBeApplication.java
@@ -0,0 +1,13 @@
+package com.basketlive.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketLiveBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasketLiveBeApplication.class, args);
+	}
+
+}
---
+++ basket-live-be/src/test/java/com/basketlive/dashboard/BasketLiveBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basketlive.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketLiveBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
